4e362992d95a1af694ccb50dd8a103722c788fb0
nicolasmarroquin <jmarroquina1@ucentral.edu.co>
2020-07-16 21:57:06 -0500

    Se agrega al modulo db el modelo Customer_sq

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const UsuarioModel  = require('./models/user')
 const MembresiaModel = require('./models/membresia')
 const EstablecimientoModel = require('./models/establecimiento')
+const Customer_sqModel = require('./models/custormer_sq')
 
 const URL = process.env.URL_DB
 const sequelize = new Sequelize(URL);
@@ -11,9 +12,11 @@ const sequelize = new Sequelize(URL);
 const User = UsuarioModel(sequelize, Sequelize)
 const Membresia = MembresiaModel(sequelize, Sequelize)
 const Establecimiento = EstablecimientoModel(sequelize, Sequelize)
+const Custormer_sq = Customer_sqModel(sequelize, Sequelize)
 
 module.exports = {
     User,
     Membresia,
-    Establecimiento
+    Establecimiento,
+    Custormer_sq
 }
