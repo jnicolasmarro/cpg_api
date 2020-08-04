68a6e464261c39432e366cfe7410a361e2f86e92
Nicolas Marroquin <jnicolas.marro@gmail.com>
2020-08-03 22:50:22 -0500

    Se agrega modelo util para permitir manejar parametrizaciones de la aplicacion

---
+++ models/util.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, type) => {
+    const Util = sequelize.define('util',{
+        id_param: {
+            type: type.INTEGER,
+            primaryKey: true
+        },
+        nombre_param:{
+            type: type.STRING
+        },
+        valor_param: {
+            type: type.INTEGER
+        }
+    }
+        , {
+            tableName: 'util',
+            timestamps: false
+      });
+    return Util;
+ }
