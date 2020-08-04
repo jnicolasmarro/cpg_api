f6cda45282ad828adb9018dc77a20d43d87994ec
Nicolas Marroquin <jnicolas.marro@gmail.com>
2020-08-03 22:17:42 -0500

    Se ajusta el modelo de la membresia para que contenga el campo de asignada

--- models/membresia.js
+++ models/membresia.js
@@ -5,14 +5,17 @@ module.exports = (sequelize, type) => {
             primaryKey: true,
             unique: true
         },
+        asignada:{
+            type: type.BOOLEAN
+        },
+        user_id_user: {
+            type: type.INTEGER
+        },
         fecha_uso: {
             type: type.DATE
         },
         fecha_vencimiento: {
             type: type.DATE
-        },
-        user_id_user: {
-            type: type.INTEGER
         }
     }
         , {
