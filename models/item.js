3e6352525813ccf1fe04d097d53386201c961ac5
Nicolas Marroquin <jnicolas.marro@gmail.com>
2020-08-05 16:25:17 -0500

    Se ajusta modelo item

--- models/item.js
+++ models/item.js
@@ -1,16 +1,18 @@
 module.exports = (sequelize, type) => {
-    const Item = sequelize.define('item',{
+    const Item = sequelize.define('item', {
         id_item: {
             type: type.INTEGER,
             primaryKey: true,
-            unique: true,
             autoIncrement: true
         },
         titulo_item: {
-            type: type.STRING
+            type: type.STRING(100)
         },
         descripcion_item: {
-            type: type.STRING
+            type: type.STRING(800)
+        },
+        estado_item: {
+            type: type.BOOLEAN
         },
         experiencia_id_experiencia_item: {
             type: type.INTEGER
@@ -19,7 +21,6 @@ module.exports = (sequelize, type) => {
         , {
             tableName: 'item',
             timestamps: true
-            
-      });
+        });
     return Item;
- }
+}
