054c70483b5a49863f76b312084181c5a0607d38
Nicolas Marroquin <jnicolas.marro@gmail.com>
2020-08-05 16:02:40 -0500

    Se realiza ajuste del modelo experiencia ajustando tamaño de los campos

--- models/experiencia.js
+++ models/experiencia.js
@@ -3,20 +3,19 @@ module.exports= (sequelize, type) => {
         id_experiencia: {
             type: type.INTEGER,
             primaryKey: true,
-            unique: true,
             autoIncrement: true
         },
         titulo_experiencia: {
-            type: type.STRING
+            type: type.STRING(150)
         },
         descripcion_experiencia: {
-            type: type.STRING
+            type: type.STRING(200)
         },
         precio_experiencia: {
-            type: type.BIGINT(15)
+            type: type.BIGINT(7)
         },
         precio_publico: {
-            type: type.BIGINT(15)
+            type: type.BIGINT(7)
         },
         estado_experiencia:{
             type: type.BOOLEAN
@@ -25,7 +24,8 @@ module.exports= (sequelize, type) => {
             type: type.DECIMAL(5, 2)
         },
         imagen_experiencia: {
-            type: type.STRING
+            type: type.STRING(150),
+            unique: true
         },
         experiencia_tipo_id_tipo: {
             type: type.INTEGER
